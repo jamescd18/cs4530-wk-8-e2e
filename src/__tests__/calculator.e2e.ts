ac03230f3dbfc08a0e4ede3ed95c097ae3b097a1
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-10-28 19:11:51 -0400

    write all e2e tests

--- src/__tests__/calculator.e2e.ts
+++ src/__tests__/calculator.e2e.ts
@@ -53,19 +53,154 @@ describe("Calculator App", () => {
     expect(displayValue).toEqual("2");
   });
 
-  it.todo('should display 0 when "1 - 1 = " is clicked');
+  it('should display 0 when "1 - 1 = " is clicked', async () => {
+    await ((await page.$(".key-1")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-subtract")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-1")) as ElementHandle<Element>).click();
 
-  it.todo('should display 4 when "2 * 2 = " is clicked');
+    await ((await page.$(".key-equals")) as ElementHandle<Element>).click();
 
-  it.todo('should display 2 when "4 / 2 = " is clicked');
+    const displayValue: string = (await (
+      await (
+        (await page.$(".calculator-display")) as ElementHandle<Element>
+      ).getProperty("textContent")
+    ).jsonValue()) as string;
+    expect(displayValue).toEqual("0");
+  });
 
-  it.todo('should display div 0 error when "1 / 0 = " is clicked');
+  it('should display 4 when "2 * 2 = " is clicked', async () => {
+    await ((await page.$(".key-2")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-multiply")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-2")) as ElementHandle<Element>).click();
 
-  it.todo('should display 15 when "1 + 2 * 7 = " is clicked');
+    await ((await page.$(".key-equals")) as ElementHandle<Element>).click();
 
-  it.todo('should display 0 when "1 + 2 - 3 = " is clicked');
+    const displayValue: string = (await (
+      await (
+        (await page.$(".calculator-display")) as ElementHandle<Element>
+      ).getProperty("textContent")
+    ).jsonValue()) as string;
+    expect(displayValue).toEqual("4");
+  });
 
-  it.todo('should display 2 when "1 + 2 / 2 - 3 * 0 = " is clicked');
+  it('should display 2 when "4 / 2 = " is clicked', async () => {
+    await ((await page.$(".key-4")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-divide")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-2")) as ElementHandle<Element>).click();
 
-  it.todo('should display 0 when "1 + 2" is clicked and then "C" is clicked');
+    await ((await page.$(".key-equals")) as ElementHandle<Element>).click();
+
+    const displayValue: string = (await (
+      await (
+        (await page.$(".calculator-display")) as ElementHandle<Element>
+      ).getProperty("textContent")
+    ).jsonValue()) as string;
+    expect(displayValue).toEqual("2");
+  });
+
+  it('should display div 0 error when "1 / 0 = " is clicked', async () => {
+    await ((await page.$(".key-1")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-divide")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-0")) as ElementHandle<Element>).click();
+
+    await ((await page.$(".key-equals")) as ElementHandle<Element>).click();
+
+    const displayValue: string = (await (
+      await (
+        (await page.$(".calculator-display")) as ElementHandle<Element>
+      ).getProperty("textContent")
+    ).jsonValue()) as string;
+    expect(displayValue).toEqual("ERR");
+  });
+
+  it('should display 15 when "1 + 2 * 7 = " is clicked', async () => {
+    await ((await page.$(".key-1")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-add")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-2")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-multiply")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-7")) as ElementHandle<Element>).click();
+
+    await ((await page.$(".key-equals")) as ElementHandle<Element>).click();
+
+    const displayValue: string = (await (
+      await (
+        (await page.$(".calculator-display")) as ElementHandle<Element>
+      ).getProperty("textContent")
+    ).jsonValue()) as string;
+    expect(displayValue).toEqual("15");
+  });
+
+  it('should display 0 when "1 + 2 - 3 = " is clicked', async () => {
+    await ((await page.$(".key-1")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-add")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-2")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-subtract")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-3")) as ElementHandle<Element>).click();
+
+    await ((await page.$(".key-equals")) as ElementHandle<Element>).click();
+
+    const displayValue: string = (await (
+      await (
+        (await page.$(".calculator-display")) as ElementHandle<Element>
+      ).getProperty("textContent")
+    ).jsonValue()) as string;
+    expect(displayValue).toEqual("0");
+  });
+
+  it('should display 2 when "1 + 2 / 2 - 3 * 0 = " is clicked', async () => {
+    await ((await page.$(".key-1")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-add")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-2")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-divide")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-2")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-subtract")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-3")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-multiply")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-0")) as ElementHandle<Element>).click();
+
+    await ((await page.$(".key-equals")) as ElementHandle<Element>).click();
+
+    const displayValue: string = (await (
+      await (
+        (await page.$(".calculator-display")) as ElementHandle<Element>
+      ).getProperty("textContent")
+    ).jsonValue()) as string;
+    expect(displayValue).toEqual("2");
+  });
+
+  it('should display 0 when "1 + 2 C" is clicked', async () => {
+    await ((await page.$(".key-1")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-add")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-2")) as ElementHandle<Element>).click();
+
+    await ((await page.$(".key-clear")) as ElementHandle<Element>).click();
+
+    const displayValue: string = (await (
+      await (
+        (await page.$(".calculator-display")) as ElementHandle<Element>
+      ).getProperty("textContent")
+    ).jsonValue()) as string;
+    expect(displayValue).toEqual("0");
+  });
+
+  it('should display 0.03125 when "0.25 * .125 = " is clicked', async () => {
+    await ((await page.$(".key-0")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-dot")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-2")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-5")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-multiply")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-dot")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-1")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-2")) as ElementHandle<Element>).click();
+    await ((await page.$(".key-5")) as ElementHandle<Element>).click();
+
+    await ((await page.$(".key-equals")) as ElementHandle<Element>).click();
+
+    const displayValue: string = (await (
+      await (
+        (await page.$(".calculator-display")) as ElementHandle<Element>
+      ).getProperty("textContent")
+    ).jsonValue()) as string;
+    expect(displayValue).toEqual("0.03125");
+  });
 });
